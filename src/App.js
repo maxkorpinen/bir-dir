55db0b5ad4570405eb937204dcf3013be077ce92
MAX KORPINEN <MAXKORPINEN@gmail.com>
2019-11-20 00:04:41 +0200

    added some components, set up routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+import List from './components/List';
+import Add from './components/Add';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+        <Navigation />
+
+        <Switch>
+          <Route exact path="/">
+            <List />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+
+      </Router>
   );
 }
 
